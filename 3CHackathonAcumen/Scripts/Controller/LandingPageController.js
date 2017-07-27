c606b8565c27e4914a6aa6aa5723f14252577b15
Meet Desai <meetdesai74@gmail.com>
2017-07-27 10:16:03 -0400

    data retrieval services

--- 3CHackathonAcumen/Scripts/Controller/LandingPageController.js
+++ 3CHackathonAcumen/Scripts/Controller/LandingPageController.js
@@ -7,5 +7,18 @@
 
     function LandingPageController() {
         var vm = this;
+        vm.search = "";
+        vm.facts = "";
+
+        vm.searchFact = searchFact;
+
+        function searchFact() {
+            return DataService.getFacts(vm.search)
+                .then(function (data) {
+                    vm.facts = data;
+                    console.log(data);
+                    return vm.facts;
+                });
+        }
     }
 })();
