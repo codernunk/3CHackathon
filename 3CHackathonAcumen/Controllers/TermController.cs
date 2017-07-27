2b2ee57a57e31f0dbdd0fb16a537fb08dabde107
frenchToasty <simpson.graham@outlook.com>
2017-07-27 13:31:59 -0400

    Added DataService added Term and Tag models

---
+++ 3CHackathonAcumen/Controllers/TermController.cs
@@ -0,0 +1,52 @@
+﻿using _3CHackathonAcumen.Classes;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _3CHackathonAcumen.Controllers
+{
+    [Authorize]
+    public class TermController : ApiController
+    {
+
+        [HttpGet]
+        [Route("api/values/upvote")]
+        public List<Term> upvote(int termId)
+        {
+            using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["3CHackathon"].ConnectionString))
+            {
+                //vulnerable to sql injection :) 
+                return db.Query<Term>("update terms set upvotes = upvotes + 1 where term_id =" + termId).ToList();
+            }
+        }
+
+        [HttpGet]
+        [Route("api/values/downvote")]
+        public List<Term> downvote(int termId)
+        {
+            using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["3CHackathon"].ConnectionString))
+            {
+                //vulnerable to sql injection :) 
+                return db.Query<Term>("update terms set views = views + 1 where term_id =" + termId).ToList();
+            }
+        }
+
+        [HttpGet]
+        [Route("api/values/addView")]
+        public List<Term> addView(int termId)
+        {
+            using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["3CHackathon"].ConnectionString))
+            {
+                //vulnerable to sql injection :) 
+                return db.Query<Term>("update terms set upvotes = upvotes - 1 where term_id =" + termId).ToList();
+            }
+        }
+    }
+}
---
+++ 3CHackathonAcumen/Scripts/Services/TermService.js
@@ -0,0 +1,23 @@
+﻿angular
+    .module("app")
+    .service("DataService", ["$http", function ($http) {
+        var urlBase = "http://localhost:61103/";
+
+        return {
+            getFacts: getFacts
+        };
+
+        function getFacts(search) {
+            return $http.get(urlBase + "api/values/searchTerm/", { params: { search: search } })
+                .then(getFactsComplete)
+                .catch(getFactsFailed);
+
+            function getFactsComplete(response) {
+                return response.data;
+            }
+
+            function getFactsFailed(error) {
+                return console.log("XHR failed" + error.data.message);
+            }
+        }
+    }]);
