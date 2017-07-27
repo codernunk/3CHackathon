d60ab53b6586f862fa759b0485799367a8f5d4c1
Meet Desai <meetdesai74@gmail.com>
2017-07-27 09:15:20 -0400

    angular added hello world

--- 3CHackathonAcumen/Controllers/ValuesController.cs
+++ 3CHackathonAcumen/Controllers/ValuesController.cs
@@ -7,7 +7,6 @@ using System.Web.Http;
 
 namespace _3CHackathonAcumen.Controllers
 {
-    [Authorize]
     public class ValuesController : ApiController
     {
         // GET api/values
---
+++ 3CHackathonAcumen/Scripts/Controller/LandingPageController.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("app")
+        .controller("LandingPageController", LandingPageController);
+
+    function LandingPageController() {
+        var vm = this;
+    }
+})();
