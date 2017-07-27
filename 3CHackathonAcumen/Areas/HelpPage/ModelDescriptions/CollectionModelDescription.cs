eb944e859cbc2ef317406d671a05c54a34f272c3
Meet Desai <meetdesai74@gmail.com>
2017-07-27 08:33:57 -0400

    Add project files.

---
+++ 3CHackathonAcumen/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace _3CHackathonAcumen.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ 3CHackathonAcumen/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_3CHackathonAcumen.WebApiApplication" Language="C#" %>
